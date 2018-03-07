f35e36945e1bed03b3911feef867a67ecc91b4be
HANK-PC\Hank <cmf15bs@yahoo.com.tw>
2018-03-08 02:34:47 +0800

    commit to Demo

---
+++ 106598024/pingHost_Project/pingHost_Project/FileOperation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pingHost_Project
+{
+    class FileOperation
+    {
+        string appath = System.IO.Directory.GetCurrentDirectory() + @"\..\..\hostList.txt";
+        public void saveFile(ConnectHostWithPing data)
+        {
+            string hostList = "";
+            for (int i = 0; i <= data.getIpListLength() - 1; i++)
+            {
+                hostList += data.makeHostList() + "\n";
+                data.next();
+            }
+            try
+            {
+                FileStream FileStream = new FileStream(appath, FileMode.Create);
+                StreamWriter sw = new StreamWriter(FileStream, System.Text.Encoding.UTF8);
+                sw.WriteLine(hostList);
+                sw.Flush();
+                sw.Close();
+                FileStream.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public string fetchFileContent()
+        {           
+            string currentFileLine = "";
+            string totalLine ="";
+            StreamReader sr = new StreamReader(appath);
+            ConnectHostWithPing Conn = new ConnectHostWithPing();
+            
+            while ((currentFileLine = sr.ReadLine()) != null)
+            {
+                totalLine += currentFileLine;
+            }
+            sr.Close();
+            return totalLine;
+        }
+        public  ConnectHostWithPing generateHostList()
+        {
+            ConnectHostWithPing conn = new ConnectHostWithPing();
+            conn.initIndex();
+            string fileContent = fetchFileContent();
+            string[] HostAndIPGroup = fileContent.Split(new string[] { "\t\t" }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i <= HostAndIPGroup.Length - 1; i += 2)
+            {
+                conn.inesertHostAndIp(HostAndIPGroup[i], HostAndIPGroup[i + 1]);
+            }
+            return conn;
+        }
+    }
+}
--- 106598024/pingHost_Project/pingHost_Project/Program.cs
+++ 106598024/pingHost_Project/pingHost_Project/Program.cs
@@ -13,38 +13,40 @@ namespace pingHost_Project
     {
         static void Main(string[] args)
         {
-            string header = string.Format("{0}\t\t{1}\t\t\t{2}","Host名稱","Ip位址","狀態");
-            string Column ="" ;
+            FileOperation file = new FileOperation();
+            ConnectHostWithPing Conn = file.generateHostList();
+            string header = string.Format("{0}\t{1}\t\t\t{2}", "Host名稱", "IPv4位址", "狀態");
+            string Column = ""; string host = "";  string ip = "";  string command = "";
+
             Console.WriteLine(header);
-            ConnectHostWithPing Conn = new ConnectHostWithPing();
             for (int i = 0; i <= Conn.getIpListLength() - 1; i++)
             {
                 Column += Conn.connectInfo() + "\n";
                 Conn.next();
             }
-            Console.WriteLine(Column);            
-            string host = "";
-            string ip = "";
-            string command ="" ;
+            Console.WriteLine(Column);
+
             while (true)
             {
                 Column = "";
-                Conn.init();
+                Conn.initIndex();
                 Console.WriteLine("\n Add Or Remove Or Ping \n");
                 command = Console.ReadLine();
-                if(command == "Add")
+                if (command == "Add")
                 {
-                    Console.Write("Host:\t");
-                    host = Console.ReadLine();
-                    Console.Write("IP:\t");
-                    ip = Console.ReadLine();
+                    Console.Write("Host:\t"); host = Console.ReadLine();
+                    Console.Write("IP:\t");   ip = Console.ReadLine();
                     Conn.inesertHostAndIp(host, ip);
+                    FileOperation csv = new FileOperation();
+                    csv.saveFile(Conn);
                 }
                 else if (command == "Remove")
                 {
-                    Console.WriteLine("輸入欲刪除的IP:");
+                    Console.Write("輸入欲刪除的IP:");
                     string removeIp = Console.ReadLine();
                     Conn.RemoveByIp(removeIp);
+                    FileOperation csv = new FileOperation();
+                    csv.saveFile(Conn);
                 }
                 else if (command == "Ping")
                 {
@@ -54,71 +56,8 @@ namespace pingHost_Project
                         Conn.next();
                     }
                     Console.WriteLine(Column);
-                }                
+                }
             }
-
-        }
-
-    }
-    class ConnectHostWithPing
-    {
-        private string[] _hostList = new string[] { "google", "NTUT" };
-        private string[] _ipList = new string[] { "216.58.200.36", "140.124.181.101"};
-        private int _currentIndex =0;
-        public void init() 
-        {
-            _currentIndex = 0;
-        }
-        public void next ()
-        {
-            _currentIndex++;
-        }
-        public int getIpListLength()
-        {
-            return _ipList.Length;
-        }
-        public string connectInfo()
-        {
-            return string.Format("{0}\t\t{1}\t\t\t{2}", _hostList[_currentIndex], _ipList[_currentIndex], pingStatus());
-        }
-        public string getIp()
-        {
-            return _ipList[_currentIndex];
-
-        }
-        public string getHostName()
-        {
-            return _hostList[_currentIndex];
-        }
-        public void inesertHostAndIp(string hostName, string ipAddress)
-        {
-            Array.Resize(ref _hostList, _hostList.Length + 1);
-            _hostList[_hostList.Length-1] = hostName;
-            Array.Resize(ref _ipList, _ipList.Length + 1);
-            _ipList[_ipList.Length-1] = ipAddress;
-        }
-        public  bool pingStatus()
-        {
-            IPAddress tIP = IPAddress.Parse(_ipList[_currentIndex]);
-            Ping tPingControl = new Ping();
-            PingReply tReply = tPingControl.Send(tIP);
-            tPingControl.Dispose();
-            if (tReply.Status != IPStatus.Success)
-                return false;
-            else
-                return true;
-        }
-
-        public void RemoveByIp(string rmIP)
-        {
-            ArrayList alToIp = new ArrayList(_ipList);
-            ArrayList alToHost  = new ArrayList(_hostList);
-            int rmIndex = alToIp.IndexOf(rmIP);
-            alToIp.RemoveAt(rmIndex);
-            string rmHost = alToHost[rmIndex].ToString();
-            alToHost.RemoveAt(rmIndex);
-            string removeInfo = string.Format("刪除項目\nHost:\t{0}\tIP{1}\t為第{2}項目",rmHost, rmIP, rmIndex+1);
-            Console.WriteLine(removeInfo);
         }
     }
 }
