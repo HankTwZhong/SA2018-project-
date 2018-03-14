4203efec42aa66c8ce147ddb0a6d54762a421af5
DESKTOP-76DUBJ1\李修豪 <firsthaze@gmail.com>
2018-03-14 21:38:04 +0800

    106598022 commit function complete

--- 106598022/ServerMonitor/routes/index.js
+++ 106598022/ServerMonitor/routes/index.js
@@ -2,23 +2,55 @@ var express = require('express');
 var router = express.Router();
 var nmap = require('node-nmap');
 nmap.nmapLocation = "nmap";
- 
+
+router.get('/', function(req, res, next) {
+  let hostArray = [];
+  const readline = require('readline').createInterface({
+    input:require('fs').createReadStream('./ServerMonitor/database.txt')
+  });
+  readline.on('line',function(line){
+    hostArray.push(line);
+  }).on('close',function(){
+    let hosts = [];
+    for(let i =0; i < hostArray.length; i++){
+      var quickscan = new nmap.QuickScan(hostArray[i]);
+      quickscan.on('complete', function(data){
+        console.log(data[0]);
+        if(data.length != 0) {
+          if (data[0].hostname == null)
+            hosts.push({hostname: '未知', ip: data[0].ip, status: 'UP'});
+          else
+            hosts.push({hostname: data[0].hostname, ip: data[0].ip, status: 'UP'});
+        } else
+            hosts.push({hostname: hostArray[i], ip: hostArray[i], status: 'Down'});
+        if(hosts.length == hostArray.length){
+          res.render("index", {hosts:hosts});
+        }
+      });
+    
+      quickscan.on('error', function(error){
+        console.log(error);
+        hosts.push({hostname:hostArray[i],hostIp:hostArray[i], status:"Down"});
+        if(hosts.length == hostArray.length){
+          res.render("index", {hosts:hosts});
+        }
+      });
+    }
+  });
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var count = 0;
-  var quickscan = new nmap.QuickScan('127.0.0.1 google.com');
+router.post('/', function(req, res, next) {
+  let hostArray = [];
+  let newHost = JSON.parse(JSON.stringify(req.body));
 
-  quickscan.on('complete', function(data){
-    console.log(data[0]);
-    count++;
-    if(count == 2)
-      res.render('index', { hostname: data[0]}); 
-  });
-
-  quickscan.on('error', function(error){
-    console.log(error);
-  });
+  const fs = require('fs');
+    fs.appendFile('./ServerMonitor/database.txt', "\n" + newHost.hostName, (err) => {  
+      if (err) throw err;
+      console.log('The database were updated!');
+      res.send("success");
+    });
+  
 });
 
 module.exports = router;
