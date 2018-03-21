73ce9705f50f09212c749ffeb5e6b6ae09af4f29
DESKTOP-PT20SU4\Chang <msps50807@gmail.com>
2018-03-21 17:42:05 +0800

    mlab mongodb

--- 106598045/SA_HW/src/main/java/DB/StorageDB.java
+++ 106598045/SA_HW/src/main/java/DB/StorageDB.java
@@ -1,5 +1,7 @@
 package DB;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.FindIterable;
@@ -11,23 +13,32 @@ import org.bson.Document;
 import org.bson.conversions.Bson;
 
 import javax.print.Doc;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import static com.mongodb.client.model.Filters.eq;
+
 public class StorageDB implements StoragePort {
+    private MongoClient mongoClient;
+    private MongoDatabase mongoDatabase;
+    private MongoCollection<Document> collection;
 
     public MongoCollection<Document> getDB(){
-        MongoClient mongoClient = new MongoClient("140.124.181.15");
-        MongoDatabase database = mongoClient.getDatabase( "MonitorSystem" );
-        MongoCollection<Document> collection = database.getCollection("host");
+        mongoClient = new MongoClient("140.124.181.15");
+        mongoDatabase = mongoClient.getDatabase( "MonitorSystem" );
+        collection = mongoDatabase.getCollection("host");
         return collection;
     }
     public MongoCollection<Document> getMlab(){
+        System.out.println(getDateTime());
         try{
             MongoClientURI uri  = new MongoClientURI("mongodb://Islab:islab1221@ds121189.mlab.com:21189/monitorsystem");
-            MongoClient client = new MongoClient(uri);
-            MongoDatabase database = client.getDatabase(uri.getDatabase());
-            MongoCollection<Document> collection = database.getCollection("host");
+            mongoClient = new MongoClient(uri);
+            mongoDatabase = mongoClient.getDatabase(uri.getDatabase());
+            collection = mongoDatabase.getCollection("host");
+            System.out.println(getDateTime());
             return collection;
         }
         catch (Exception e){
@@ -36,14 +47,18 @@ public class StorageDB implements StoragePort {
         }
     }
     public void addHost(Host host){
-        MongoCollection<Document> collection = getMlab();
+        System.out.println("addHost start " + getDateTime());
+        collection = getMlab();
         Document doc =new Document("hostName", host.getHostName())
                 .append("hostIp", host.getHostIp());
         collection.insertOne(doc);
+        mongoClient.close();
+        System.out.println("addHost finish " + getDateTime());
     }
     public List<Host> getHost(){
+        System.out.println("getHost start " + getDateTime());
         List<Host> hostList = new ArrayList<Host>();
-        MongoCollection<Document> collection = getMlab();
+        collection = getMlab();
 
         FindIterable<Document> myDoc = collection.find();
         for (Document document : myDoc) {
@@ -52,11 +67,21 @@ public class StorageDB implements StoragePort {
             host.setHostIp(document.get("hostIp").toString());
             hostList.add(host);
         }
+        mongoClient.close();
+        System.out.println("getHost finish " + getDateTime());
         return hostList;
     }
     public void deleteHost(String hostIp){
-        MongoCollection<Document> collection = getMlab();
-        Bson deleteHost = Filters.eq("hostIp", hostIp);
-        collection.deleteOne(deleteHost);
+        System.out.println("deleteHost start " + getDateTime());
+        collection = getMlab();
+        collection.deleteOne(eq("hostIp", hostIp));
+        mongoClient.close();
+        System.out.println("deleteHost finish " + getDateTime());
+    }
+    public String getDateTime(){
+        SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+        Date date = new Date();
+        String strDate = sdFormat.format(date);
+        return strDate;
     }
 }
