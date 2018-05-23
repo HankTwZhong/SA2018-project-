4c5da43a6e00e615524e6cf2c6ae3f2bdb2c548b
吳彥銘 <40143109@gm.nfu.edu.tw>
2018-05-24 00:37:54 +0800

    fake

---
+++ 106598060/server/test/utils/FakeHostRepository.js
@@ -0,0 +1,8 @@
+export default class FakeHostRepository{
+    static saveHost(hostList){
+        console.log('fake add success')
+    }
+    static deleteHost(hostList){
+        console.log('fake delete success')
+    }
+}
